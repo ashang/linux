4f7bc45653c63a91ffc892835d8aad2dcda14b2d
vic.lan <vic.lan@pica8.local>
2021-07-23 10:52:51 +0800

    frr: add ipv6 ioctrl for pim keep alive timer.

--- include/uapi/linux/mroute6.h
+++ include/uapi/linux/mroute6.h
@@ -36,6 +36,7 @@
 #define SIOCGETMIFCNT_IN6	SIOCPROTOPRIVATE	/* IP protocol privates */
 #define SIOCGETSGCNT_IN6	(SIOCPROTOPRIVATE+1)
 #define SIOCGETRPF	(SIOCPROTOPRIVATE+2)
+#define SIOCSETSGCNT_IN6	(SIOCPROTOPRIVATE+3) /* 0x89E3 */
 
 #define MAXMIFS		32
 typedef unsigned long mifbitmap_t;	/* User mode code depends on this lot */
